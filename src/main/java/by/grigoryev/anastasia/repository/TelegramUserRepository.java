a1789a57cdecb5245cf1469651d5587141349c9e
Grigoryev Pavel <Green-ono@yandex.ru>
2022-11-24 16:28:39 +0300

    added findFirstByTelegramIdOrderByTimeOfRegistrationDesc() method

--- src/main/java/by/grigoryev/anastasia/repository/TelegramUserRepository.java
+++ src/main/java/by/grigoryev/anastasia/repository/TelegramUserRepository.java
@@ -3,8 +3,11 @@ package by.grigoryev.anastasia.repository;
 import by.grigoryev.anastasia.model.TelegramUser;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface TelegramUserRepository extends ReactiveCrudRepository<TelegramUser, Long> {
+
+    Mono<TelegramUser> findFirstByTelegramIdOrderByTimeOfRegistrationDesc(Long telegramId);
 
 }
--- src/main/java/by/grigoryev/anastasia/service/impl/AnswerServiceImpl.java
+++ src/main/java/by/grigoryev/anastasia/service/impl/AnswerServiceImpl.java
@@ -2,6 +2,7 @@ package by.grigoryev.anastasia.service.impl;
 
 import by.grigoryev.anastasia.model.Answer;
 import by.grigoryev.anastasia.repository.AnswerRepository;
+import by.grigoryev.anastasia.repository.TelegramUserRepository;
 import by.grigoryev.anastasia.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,24 +10,27 @@ import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.User;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
-
 @Service
 @RequiredArgsConstructor
 public class AnswerServiceImpl implements AnswerService {
 
     private final AnswerRepository answerRepository;
 
+    private final TelegramUserRepository telegramUserRepository;
+
     @Override
     public Mono<Answer> save(CallbackQuery callbackQuery, String message) {
         User user = callbackQuery.getFrom();
-        Answer answer = Answer.builder()
-                .message(message)
-                .timeOfAnswer(LocalDateTime.now())
-                .telegramUserId(user.getId())
-                .build();
 
-        return answerRepository.save(answer)
+        return telegramUserRepository.findFirstByTelegramIdOrderByTimeOfRegistrationDesc(user.getId())
+                .flatMap(telegramUser -> {
+                    Answer answer = Answer.builder()
+                            .message(message)
+                            .timeOfAnswer(telegramUser.getTimeOfRegistration())
+                            .telegramUserId(user.getId())
+                            .build();
+                    return answerRepository.save(answer);
+                })
                 .log("AnswerServiceImpl save");
     }
 }
